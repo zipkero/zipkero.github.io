83f395722bde1ab339ed42c12cbaec4fecde8f71
zipkero <zipkero@gmail.com>
2023-06-25 17:49:17 +0900

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const ContentSecurityPolicy = `
   media-src 'none';
   connect-src *;
   font-src 'self' data:;
-  frame-src giscus.app
+  frame-src 'self' https://utteranc.es giscus.app;
 `
 
 const securityHeaders = [
